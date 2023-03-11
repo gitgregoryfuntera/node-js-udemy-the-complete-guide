f8b89cdbc6de00d6ccffe2aa6a68f1e23ece878d
gitgregoryfuntera <gregory.funtera@gmail.com>
2023-03-11 09:23:10 +0800

    - added other paths

--- app.js
+++ app.js
@@ -1,9 +1,48 @@
-const http = require('http')
+const http = require("http");
+const fs = require("fs")
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-    console.log(req)
-})
+  const { headers, method, url } = req;
 
-server.listen(PORT)
+  if (url === "/") {
+    res.setHeader(`Content-Type`, "text/html");
+    res.write(
+      `<html>
+          <head>
+            <title>Enter Message</title>
+          </head>
+          <body>
+            <form action="/message" method="POST">
+              <input type="text" placeholder="enter message here..."/>
+              </br>
+              </br>
+              <button type="submit">submit</button>
+            </form>
+          </body>
+       </html>
+      `
+    );
+    return res.end();
+  }
+
+  if (url === '/message' && method === 'POST') {
+    fs.writeFileSync('message.txt', 'DUMMY TXT')
+    res.statusCode = 302
+    res.setHeader('Location', '/')
+    return res.end();
+  }
+
+  res.setHeader("Content-Type", "text/html");
+  res.write(`<html>
+        <head>
+        <title>My First Title</title></head>
+        <body>
+            <h1>Hello from Node JS</h1>
+        </body>
+    </html>`);
+  res.end();
+});
+
+server.listen(PORT);
