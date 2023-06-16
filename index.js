e429b2f605173930ad219c6610b0ffa6ed08e0ae
gitgregoryfuntera <gregory.funtera@gmail.com>
2023-06-17 06:53:32 +0800

    section 5: modified app

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
-const http = require("http");
 const express = require("express");
 const PORT = 8080;
 
 const app = express();
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+  console.log(`middleware`);
+  next();
+});
 
 app.use((req, res, next) => {
-    console.log(`middleware`)
-    next()
-})
+  console.log(`middleware another`);
+  res.send(`<h1>Hello from Node</h1>`);
+});
 
-app.use((req, res, next) => {
-    console.log(`middleware another`)
-    res.send(`<h1>Hello from Node</h1>`)
-})
-
-server.listen(PORT, () => {
-  console.log(`server listens to port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`App is listening to PORT: ${PORT}`);
 });
