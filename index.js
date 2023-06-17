0b50a22e160ad1ae458383f445b3bd4a4ed054be
gitgregoryfuntera <gregory.funtera@gmail.com>
2023-06-18 07:24:26 +0800

    section 5: added middleware samples section 5: added route samples

--- index.js
+++ index.js
@@ -3,15 +3,27 @@ const PORT = 8080;
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log(`middleware`);
-  next();
+// app.use((req, res, next) => {
+//   console.log('First Middleware')
+//   next()
+// })
+
+// app.use((req, res, next) => {
+//   console.log('Second Middleware')
+//   res.send(`<h2>Second Middleware</h2>`)
+// })
+
+app.use(`/users`, (req, res, next) => {
+  console.log(`users`);
+  res.send(`<h1>Hello Users</h1>`);
 });
 
-app.use((req, res, next) => {
-  console.log(`middleware another`);
-  res.send(`<h1>Hello from Node</h1>`);
+app.use(`/`, (req, res, next) => {
+  console.log(`home`);
+  res.send(`<h1>Hello Home</h1>`);
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`App is listening to PORT: ${PORT}`);
