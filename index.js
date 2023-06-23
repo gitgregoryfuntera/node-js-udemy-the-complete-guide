757ef28e6ee46a8ebd07647af1a5290c03ab98a2
gitgregoryfuntera <gregory.funtera@gmail.com>
2023-06-23 08:25:27 +0800

    section 5: added 404 page fallback

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use(adminRoutes)
 
 app.use(userRoutes)
 
+app.use((req, res, next) => {
+  res.status(404).send(`<h1>Page Not Found</h1>`)
+})
+
 app.listen(PORT, () => {
   console.log(`App is listening to PORT: ${PORT}`);
 });
