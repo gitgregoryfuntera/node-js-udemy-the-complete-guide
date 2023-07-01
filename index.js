e3dfbf395f2c617b8068197581bdff597b060355
gitgregoryfuntera <gregory.funtera@gmail.com>
2023-07-01 08:38:20 +0800

    section 5: added util for path

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
 const userRoutes = require("./routes/user");
@@ -13,7 +14,7 @@ app.use("/admin", adminRoutes);
 app.use(userRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send(`<h1>Page Not Found</h1>`);
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(PORT, () => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,14 @@
 const path = require('path')
 const { Router } = require("express");
-
+const rootDir = require('../util/path')
 const router = Router();
 
 router.get(`/add-user`, (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-user.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-user.html'));
 });
 
 router.post(`/user`, (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'user.html'));
+  res.sendFile(path.join(rootDir, 'views', 'user.html'));
 });
 
 module.exports = router;
