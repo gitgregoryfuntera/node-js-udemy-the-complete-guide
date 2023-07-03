fc2a231915ca0a2d6869adac1c7a6298c7e5955c
gitgregoryfuntera <gregory.funtera@gmail.com>
2023-07-03 17:25:00 +0800

    section-5: added static files

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ app.use("/admin", adminRoutes);
 
 app.use(userRoutes);
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
