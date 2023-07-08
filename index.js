9499ed862ca97b92f3ba11893676c3bd159e7ac6
gitgregoryfuntera <gregory.funtera@gmail.com>
2023-07-08 09:55:45 +0800

    section-6: added template engine packages section-6: added PUG

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const PORT = 8080;
 
 const app = express();
 
+app.set('view engine', 'pug')
+app.set('views', 'views')
+
 app.use(bodyParser.urlencoded());
 
 app.use("/admin", adminRoutes.router);
--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,12 @@ const path = require('path')
 const rootDir = require('../util/path')
 const router = Router();
 const adminData = require('./admin')
+
 router.get(`/`, (req, res, next) => {
-  console.log(adminData.users, 'users.js')
-  res.sendFile(path.join(rootDir, 'views', 'user.html'));
+  // console.log(adminData.users, 'users.js')
+
+  // res.sendFile(path.join(rootDir, 'views', 'user.html'));
+  res.render('user')
 });
 
 module.exports = router;
