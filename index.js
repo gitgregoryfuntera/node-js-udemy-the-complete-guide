b37b6ca2078a3ac9d3d71118e8e9c0e0c643d8f0
gitgregoryfuntera <gregory.funtera@gmail.com>
2023-07-12 18:07:52 +0800

    section-6: added handlebars engine

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const adminRoutes = require("./routes/admin");
 const userRoutes = require("./routes/user");
+const handlebars = require('express-handlebars')
 const PORT = 8080;
 
 const app = express();
 
-app.set('view engine', 'pug')
+app.engine('hbs', handlebars())
+app.set('view engine', 'hbs')
+// app.set('view engine', 'pug')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded());
