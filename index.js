69ffe45bc48e6b14ba6c038070b4d2f721ec58a0
gitgregoryfuntera <gregory.funtera@gmail.com>
2023-07-16 08:28:16 +0800

    section-6: added ejs includes views

--- index.js
+++ index.js
@@ -29,7 +29,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use((req, res, next) => {
   // res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
   res.status(404).render('404', {
-    docTitle: 'Page Not Found'
+    docTitle: 'Page Not Found',
+    path: '/404'
   })
 });
 
