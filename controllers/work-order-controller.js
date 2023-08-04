a2379b38d924d618c9262f62555524da1cac3d9b
gitgregoryfuntera <gregory.funtera@gmail.com>
2023-08-05 06:35:42 +0800

    section-9: added work orders

--- controllers/work-order-controller.js
+++ controllers/work-order-controller.js
@@ -1,12 +1,24 @@
 const getWorkOrders = async (req, res, next) => {
-    const tasks = [];
-    res.render("users/work-order", {
-      tasks,
-      docTitle: "Work Orders",
-      path: "/work-order",
-    });
-  };
-  
-  module.exports = {
-    getWorkOrders,
-  };
+  res.render("users/work-order", {
+    docTitle: "Work Orders",
+    path: "/work-order",
+  });
+};
+
+const postAddWorkOrder = async (req, res, next) => {
+  const {
+    body: { user_id, work_order_id, work_order_name },
+  } = req;
+  console.log("🚀 ~ file: work-order-controller.js:11 ~ postAddWorkOrder ~ work_order_name:", work_order_name);
+  console.log("🚀 ~ file: work-order-controller.js:11 ~ postAddWorkOrder ~ work_order_id:", work_order_id);
+  console.log("🚀 ~ file: work-order-controller.js:11 ~ postAddWorkOrder ~ user_id:", user_id);
+  res.render("users/work-order", {
+    docTitle: "Work Orders",
+    path: "/work-order",
+  });
+};
+
+module.exports = {
+  getWorkOrders,
+  postAddWorkOrder,
+};
